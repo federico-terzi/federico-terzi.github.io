6d87526d4eda70a88dd19cf01342c28fd19a366a
federico-terzi <federico-terzi@users.noreply.github.com>
2022-02-04 21:51:29 +0000

    deploy: b4b7b3e328e4bc0001c7f81099cf7901c718f405

--- _nuxt/static/1644007862/blog/understanding-strings-in-rust/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644007862",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Funderstanding-strings-in-rust",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e8687842"}}}}(null));
