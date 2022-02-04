dd3e6618a6e95d9f5a85860a2e131ada70a9d78c
federico-terzi <federico-terzi@users.noreply.github.com>
2022-02-04 20:11:22 +0000

    deploy: 6e915d2ff8b6450e852a910bbb9355dd9dc2510c

---
+++ _nuxt/static/1644005480/blog/learning-how-to-type-properly-makes-you-a-better-programmer/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644005480",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Flearning-how-to-type-properly-makes-you-a-better-programmer",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5f1b180d"}}}}(null));
