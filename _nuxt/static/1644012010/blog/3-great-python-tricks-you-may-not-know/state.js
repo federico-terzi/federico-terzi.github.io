08e20a6ae5feb7975c60e60dbbdf9609d782aeb7
federico-terzi <federico-terzi@users.noreply.github.com>
2022-02-26 19:22:17 +0000

    deploy: 5a843db0ea2a5b6f7e31c05ba5dffde8e58bd260

--- _nuxt/static/1644012010/blog/3-great-python-tricks-you-may-not-know/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644012010",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002F3-great-python-tricks-you-may-not-know",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bad4d43e"}}}}(null));
--- _nuxt/static/1644012010/blog/rust-vs-cpp-which-is-better-and-why/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644012010",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Frust-vs-cpp-which-is-better-and-why",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bad4d43e"}}}}(null));
