86f311aebca21a21d7074493c5bb02f5515a51cf
federico-terzi <federico-terzi@users.noreply.github.com>
2022-05-02 20:44:43 +0000

    deploy: ede10f6cbd1a4058edf0087a5bbc4e5af20656b1

---
+++ _nuxt/static/1651524279/books/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1651524279",layout:"default",error:a,serverRendered:true,routePath:"\u002Fbooks",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7fab8c04"}}}}(null));
