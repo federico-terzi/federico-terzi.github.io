763520090c34a3233fce70476dd9fa6ce46bcb7d
federico-terzi <federico-terzi@users.noreply.github.com>
2022-08-21 13:20:16 +0000

    deploy: 7bdfee42e11405c39de79bb3960cee06c160d7f8

--- _nuxt/static/1660574512/blog/12-rust-tips-and-tricks-you-might-not-know-yet/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1660574512",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002F12-rust-tips-and-tricks-you-might-not-know-yet",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"48156a2c"}}}}(null));
