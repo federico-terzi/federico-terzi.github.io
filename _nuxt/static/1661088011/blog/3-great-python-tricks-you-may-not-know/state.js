8c83268a3d88dada3ee21ccf6bce45743c7d4b52
federico-terzi <federico-terzi@users.noreply.github.com>
2022-08-27 17:48:12 +0000

    deploy: b6dbe2a960f31e6e1889bb2e9b7565094a6c80d0

--- _nuxt/static/1661088011/blog/3-great-python-tricks-you-may-not-know/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1661088011",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002F3-great-python-tricks-you-may-not-know",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"38674c92"}}}}(null));
---
+++ _nuxt/static/1661622488/blog/12-rust-tips-and-tricks-you-might-not-know-yet/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1661622488",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002F12-rust-tips-and-tricks-you-might-not-know-yet",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2d80bfbd"}}}}(null));
