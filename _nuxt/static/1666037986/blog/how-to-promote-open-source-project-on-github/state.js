8148dfc91d791df37346742c971e1e86324d2d73
federico-terzi <federico-terzi@users.noreply.github.com>
2022-10-17 20:19:53 +0000

    deploy: 32c83023ad1a5bcff72a918e043cdb5014bce82c

---
+++ _nuxt/static/1666037986/blog/how-to-promote-open-source-project-on-github/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1666037986",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fhow-to-promote-open-source-project-on-github",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9cd739bc"}}}}(null));
