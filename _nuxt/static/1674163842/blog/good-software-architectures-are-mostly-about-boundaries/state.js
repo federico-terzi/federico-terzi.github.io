2e794f034a6bc4b49e64b3ec814430898cc8f338
federico-terzi <federico-terzi@users.noreply.github.com>
2023-04-30 11:02:10 +0000

    deploy: f2124ae9847647fa0a896b3aa39b2aac8376efb1

--- _nuxt/static/1674163842/blog/good-software-architectures-are-mostly-about-boundaries/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1674163842",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fgood-software-architectures-are-mostly-about-boundaries",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"43844537"}}}}(null));
