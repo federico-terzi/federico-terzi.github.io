8716da836870e2a52e91a94ca6157fad6e32b4b1
federico-terzi <federico-terzi@users.noreply.github.com>
2024-01-19 14:36:24 +0000

    deploy: ab70b042eded702fc17fbe6d8dba9e06e24a24bd

--- _nuxt/static/1705674734/blog/fixing-the-expecting-trusted-certificate-error-with-sectigo-certificates/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1705674734",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Ffixing-the-expecting-trusted-certificate-error-with-sectigo-certificates",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"38a45eb8"}}}}(null));
