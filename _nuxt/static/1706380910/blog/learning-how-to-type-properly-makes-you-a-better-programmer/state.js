5a2c17178fc2743640ec9e2544435131c780b91f
federico-terzi <federico-terzi@users.noreply.github.com>
2024-01-27 18:41:54 +0000

    deploy: 961110fe00d5281c149d8f627f3a76c8dd53d1cb

---
+++ _nuxt/static/1706380910/blog/learning-how-to-type-properly-makes-you-a-better-programmer/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1706380910",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Flearning-how-to-type-properly-makes-you-a-better-programmer",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3052f615"}}}}(null));
---
+++ _nuxt/static/1706380910/blog/turn-telegram-into-a-static-web-server-with-tlgur/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1706380910",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fturn-telegram-into-a-static-web-server-with-tlgur",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3052f615"}}}}(null));
