35585a3bbdbe687bc066f1f5f54ce49e0adfc6a4
federico-terzi <federico-terzi@users.noreply.github.com>
2024-09-09 15:44:35 +0000

    deploy: 83391752e7b685e525c91e894248025d84a83226

--- _nuxt/static/1712253390/blog/why-electron-is-a-necessary-evil/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1712253390",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fwhy-electron-is-a-necessary-evil",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"19b0553c"}}}}(null));
--- _nuxt/static/1712253390/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1712253390",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"19b0553c"}}}}(null,"\u002F"));
