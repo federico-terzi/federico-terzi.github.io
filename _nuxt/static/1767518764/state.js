9fda9e7d197ba923bab077ccf448ca24895c2b24
federico-terzi <federico-terzi@users.noreply.github.com>
2026-01-04 09:26:08 +0000

    deploy: 9d5616cc15b91e40ca1b826dec747b4617e064f8

---
+++ _nuxt/static/1767518764/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1767518764",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e61406b7"}}}}(null,"\u002F"));
